c02d17a836fbbe61aac4b9036f0b079b151cfad9
Natashe4ka <90340759+Natashe4ka@users.noreply.github.com>
2023-04-05 11:29:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 965404933 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 88
+
+    D = (sum([(x[i]-x.mean())**2 for i in range(x.shape[0])]))/(x.shape[0]-1)
+
+    return D/t # Ваш ответ
